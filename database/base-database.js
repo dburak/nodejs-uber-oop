f018747422fa39141cc6bacf9f090c1324f4532d
Burak Diker <burak.diker@ug.bilkent.edu.tr>
2022-08-11 21:33:20 +0300

    added-async-await

--- database/base-database.js
+++ database/base-database.js
@@ -6,36 +6,65 @@ class BaseDatabase {
     this.model = model;
     this.filename = model.name;
   }
+
   save(objects) {
-    fs.writeFileSync(`./${this.filename}.json`, flatted.stringify(objects));
+    return new Promise((resolve, reject) => {
+      fs.writeFile(
+        `${this.filename}.json`,
+        flatted.stringify(objects, null, 2),
+        (err) => {
+          if (err) return reject(err);
+          resolve();
+        }
+      );
+    });
   }
 
   load() {
-    const file = fs.readFileSync(`./${this.filename}.json`, "utf-8");
-    const objects = flatted.parse(file);
+    return new Promise((resolve, reject) => {
+      fs.readFile(`${this.filename}.json`, "utf8", (err, file) => {
+        if (err) return reject(err);
 
-    return objects.map(this.model.create);
+        const objects = flatted.parse(file);
+
+        resolve(objects.map(this.model.create));
+      });
+    });
   }
 
-  insert(object) {
-    const objects = load();
-    this.save(filename, objects.concat(object));
+  async insert(object) {
+    const objects = await this.load();
+    return this.save(objects.concat(object));
   }
 
   remove(index) {
     const objects = this.load();
+
     objects.splice(index, 1);
     this.save(objects);
   }
+
   update(object) {
     const objects = this.load();
+
     const index = objects.findIndex((o) => o.id == object.id);
+
     if (index == -1)
       throw new Error(
         `Cannot find ${this.model.name} instance with id ${object.id}`
       );
+
     objects.splice(index, 1, object);
     this.save(objects);
+  }
+
+  async find(id) {
+    const objects = await this.load();
+    return objects.find((o) => o.id == id);
+  }
+
+  async findBy(property, value) {
+    return (await this.load()).find((o) => o[property] == value);
   }
 }
 
--- index.js
+++ index.js
@@ -1,40 +1,30 @@
 const Passenger = require("./passenger");
 const Driver = require("./driver");
-const {passengerDatabase, driverDatabase} = require('./database')
-const { v4: uuidv4 } = require('uuid');
+const { passengerDatabase, driverDatabase } = require("./database");
+const { v4: uuidv4 } = require("uuid");
+const printBookingHistory = require("./lib/print-booking-history");
 
-// const burak = new Passenger(uuidv4(), "Burak", "Bahçeli 7.cadde");
+const burak = new Passenger(uuidv4(), "Burak", "Bahçeli 7.cadde");
 const ahmet = new Driver(uuidv4(), "Ahmet", "Milli Kütüphane");
 
-// burak.book(ahmet, "Bahçeli 7.cadde", "Armada AVM");
-// burak.book(ahmet, "Armada AVM", "Kentpark AVM");
-// burak.book(ahmet, "Kentpark AVM", "Bahçeli 7.cadde");
+burak.book(ahmet, "Bahçeli 7.cadde", "Armada AVM");
+burak.book(ahmet, "Armada AVM", "Kentpark AVM");
+burak.book(ahmet, "Kentpark AVM", "Bahçeli 7.cadde");
 
-function printBooking(booking) {
-  console.log(
-    `${booking.passenger.name} booked to ${booking.driver.name} to travel from ${booking.origin} to ${booking.destination}`
-  );
-}
+async function main() {
+  try {
+    await passengerDatabase.save([burak])
 
-function printBookingHistory(passenger) {
-  if (passenger.bookings.length == 0) {
-    return console.log(`${passenger.name} has no bookings yet`);
+    await driverDatabase.save([ahmet])
+
+    const aslı = Passenger.create({id: uuidv4(), name: 'Aslı', location: 'Bilkent' })
+
+    await passengerDatabase.insert(aslı)
+    const passengers = await passengerDatabase.load()
+    passengers.forEach(printBookingHistory)
+  } catch (e) {
+    return console.log(e)
   }
-  passenger.bookings.forEach(printBooking);
 }
 
-// passengerDatabase.save([burak]);
-
-const foundName = passengerDatabase.findByName("Burak");
-
-foundName.book(ahmet, "Bilkent", "ODTU");
-
-passengerDatabase.update(foundName);
-
-
-printBookingHistory(foundName);
-
-
-
-
-// passengers.forEach((p) => console.log(p.name));
+main()
